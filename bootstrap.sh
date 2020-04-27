bd44dcea4d6ce0d880132317d4071a78ac510fd6
Aaron Schutza <aaronschutza@gmail.com>
2020-04-27 14:00:33 -0500

    bootstrap.conf changes #33

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,18 +6,8 @@ if read -t 10 response; then
 else
     stakeHolderIndex=$(((RANDOM<<15)|RANDOM))
 fi
-#echo -n "Enter known peer IP address > "
-#if read -t 10 response; then
-#    knownPeer=$response
-#else
-#    knownPeer=""
-#fi
 echo "Holder Index = $stakeHolderIndex"
-#PORT=$(( ((RANDOM<<15)|RANDOM) % 63001 + 2000 ))
-#echo $PORT
-#BIND="127.$((RANDOM % 256)).$((RANDOM % 256)).$((RANDOM % 256))"
 BIND="0.0.0.0"
-#echo $BIND
 sbt "run-main prosomo.Prosomo bootstrap.conf \
 input{params{\
 inputSeed=\"prosomo_testnet\",\
@@ -25,7 +15,6 @@ settingsFilename=\"bootstrap.json\",\
 numHolders=$numHolders,\
 holderIndexMin=$stakeHolderIndex,\
 holderIndexMax=$stakeHolderIndex,\
-knownPeer=\"$knownPeer\",\
 dataFileDir=data_$stakeHolderIndex,\
 rpcPort=9085,\
 bindAddress=$BIND\
