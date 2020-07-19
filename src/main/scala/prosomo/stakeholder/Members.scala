b95098c8e576eef2b74ff8c6170b6309df8c3fb8
Aaron Schutza <aaronschutza@gmail.com>
2020-07-19 14:30:40 -0500

    test

--- src/main/scala/prosomo/stakeholder/Members.scala
+++ src/main/scala/prosomo/stakeholder/Members.scala
@@ -24,41 +24,16 @@ trait Members extends SimpleTypes with Actor with Timers {
 
   val devMode:Boolean = Parameters.devMode
   var useGui:Boolean = Parameters.useGui
-  val timeScale:Double = Parameters.timeScale
   val dataBaseCID: ByteArrayWrapper = Parameters.dataBaseCID
   val genesisBytes: ByteArrayWrapper = Parameters.genesisBytes
   val inputSeedString:String = Parameters.inputSeedString
-  //use network delay parameterization if true
-  val useDelayParam:Boolean = Parameters.useDelayParam
-  //number of stakeholders
   val numGenesisHolders:Int = Parameters.numGenesisHolders
-  //minumum index of local holders, set to -1 for all local
   val holderIndexMin:Int = Parameters.holderIndexMin
-  //maximum index of local holders, set to -1 for all local
   val holderIndexMax:Int = Parameters.holderIndexMax
-  //duration of slot in milliseconds
   val slotT:Long = Parameters.slotT
-  //interval between return blocks in tine provider
-  val requestTineInterval:Int = Parameters.requestTineInterval
-  //delay in milliseconds per kilometer in router model
-  val delay_ms_km:Double = Parameters.delay_ms_km
-  //delay in milliseconds per bit in router model
-  val delay_ms_byte:Double = Parameters.delay_ms_byte
-  //network random noise max
-  val delay_ms_noise:Double = Parameters.delay_ms_noise
-  //communication method
   val useRouting:Boolean = Parameters.useRouting
-  //delay in slots, calculated as maximum possible delay in random global network model
   val delta_s:Int = Parameters.delta_s
-  //epoch parameter
-  val epsilon_s:Double = Parameters.epsilon_s
-  //alert stake ratio
-  val alpha_s:Double = Parameters.alpha_s
-  //participating stake ratio
-  val beta_s:Double = Parameters.beta_s
-  //active slot coefficient
   val f_s:Double = Parameters.f_s
-  //order of accuracy for convergent series
   val o_n:Int = Parameters.o_n
   val m_f_root:Ratio = Parameters.m_f_root
   val f_dynamic:Boolean = Parameters.f_dynamic
@@ -68,73 +43,31 @@ trait Members extends SimpleTypes with Actor with Timers {
   val num_f:Int = Parameters.num_f
   val m_f_range:Array[Ratio] = Parameters.m_f_range
   val k_s:Int = Parameters.k_s
-  // epoch length R >= 3k/2f
   val epochLength:Int = Parameters.epochLength
   val one_third_epoch:Int = Parameters.one_third_epoch
-  // slot window for chain selection, s = k/4f
   val slotWindow:Int = Parameters.slotWindow
-  //status and verify check chain hash data up to this depth to gauge consensus amongst actors
   val confirmationDepth:Int = Parameters.confirmationDepth
-  //max initial stake
   val initStakeMax:Double = Parameters.initStakeMax
-  //max random transaction delta
   val maxTransfer:Double = Parameters.maxTransfer
-  //reward for forging blocks
   val forgerReward:BigInt = Parameters.forgerReward
-  //percent of transaction amount taken as fee by the forger
-  val transactionFee:Double = Parameters.transactionFee
   val fee_r:Ratio = Parameters.fee_r
-  //number of holders on gossip list for sending new blocks and transactions
   val numGossipers:Int = Parameters.numGossipers
-  //number of holders to gossip to upon forging
   val numGossipersForge:Int = Parameters.numGossipersForge
-  //max number of tries for a tine to ask for parent blocks
   val tineMaxTries:Int = Parameters.tineMaxTries
-  //max depth in multiples of confirmation depth that can be returned from an actor
   val tineMaxDepth:Int = Parameters.tineMaxDepth
-  //data write interval in slots
   val dataOutInterval:Int = Parameters.dataOutInterval
-  //time out for dropped messages from coordinator
   val waitTime:FiniteDuration = Parameters.waitTime
-  //duration between update tics that stakeholder actors send to themselves
   val updateTime:FiniteDuration = Parameters.updateTime
-  //duration between command read tics and transaction generation for the coordinator
   val commandUpdateTime:FiniteDuration = Parameters.commandUpdateTime
-  //number of txs per block
   val txPerBlock:Int = Parameters.txPerBlock
-  //Issue random transactions if true
   var transactionFlag:Boolean = Parameters.transactionFlag
-  // p = txProbability => (1-p)^numHolders
   var txProbability:Double = Parameters.txProbability
-  //uses randomness for public key seed and initial stake, set to false for deterministic run
-  val randomFlag:Boolean = Parameters.randomFlag
-  //when true, if system cpu load is too high the coordinator will stall to allow stakeholders to catch up
-  val performanceFlag:Boolean = Parameters.performanceFlag
-  //threshold of cpu usage above which coordinator will stall if performanceFlag = true
-  val systemLoadThreshold:Double = Parameters.systemLoadThreshold
-  //number of values to average for load threshold
-  val numAverageLoad:Int = Parameters.numAverageLoad
-  //print Stakeholder 0 status per slot if true
   val printFlag:Boolean = Parameters.printFlag
-  //print Stakeholder 0 execution time per slot if true
   val timingFlag:Boolean = Parameters.timingFlag
-  //Record data if true, plot data points with ./cmd.sh and enter command: plot
   val dataOutFlag:Boolean = Parameters.dataOutFlag
-  //toggle for action based round execution
   val useFencing:Boolean = Parameters.useFencing
-  //interval in slots between localChain save to disk
   val chainStoreInterval:Int = Parameters.chainStoreInterval
-  //number of message processors
-  val numMessageProcessors:Int = Parameters.numMessageProcessors
-  //node secret for HMAC used in each router actor
-  val sk_ecx:Array[Byte] = Parameters.sk_ecx
-  //public key for HMAC used in each router actor
-  val pk_ecx:Array[Byte] = Parameters.pk_ecx
-
-  //path for data output files
   val dataFileDir:String = Parameters.dataFileDir
-  val storageFlag:Boolean = Parameters.storageFlag
-  val cacheSize:Int = Parameters.cacheSize
   val refreshInterval:Int = Parameters.refreshInterval
   val stakeDistribution:String = Parameters.stakeDistribution
   val stakeScale:Double = Parameters.stakeScale
