2a1956414b2b9decf86f9ce7765001115e8544af
Aaron Schutza <aaronschutza@gmail.com>
2021-04-07 15:06:36 -0500

    Made challenger extensions optional, added reserve

--- src/main/python/branchingPlot.py
+++ src/main/python/branchingPlot.py
@@ -11,12 +11,12 @@ import matplotlib.cm as cm
 # Honest extensions assume 1-alpha resources split among each maximum length branch with distinct parent slots.
 
 # Set seed for reproducibility
-# np.random.seed(123)
+#np.random.seed(123)
 
 # Number of Slots
-N = 200
+N = 100
 # Slots
-x = np.arange(N)
+slots = np.arange(N)
 # Adversarial nonces
 ys = np.random.rand(N)
 # Proportion of adversarial stake
@@ -27,101 +27,123 @@ gamma = 15
 fa = 0.5
 # Set slope of snowplow
 c = 0.0
-if gamma>0:
-    c = fa/gamma
+if gamma > 0:
+    c = fa / gamma
 # Baseline difficulty
 fb = 0.05
 # Maximum difference between leading block number and viable branches
 # branches with a gap greater than branchDepth are cut
-branchDepth = 4
+branchDepth = 3
+# use challenger model, honest extensions
+useChallenger = False
 # calculate plots
 makePlots = True
 
+
 # Snowplow curve
 def f(d):
-    if d < gamma+1:
-        return c*d
+    if d < gamma + 1:
+        return c * d
     else:
         return fb
-       
+
+
 # Staking threshold       
-def phi(d,a):
-    return 1.0 - (1.0-f(d))**a
+def phi(d, a):
+    return 1.0 - (1.0 - f(d)) ** a
+
 
 # Nonce distribution plot area and colors
 area = []
 colors = []
 
+
 # Generates the spectrum of colors for trails in branching diagram
 def genSpectrum(g):
-    if g>0:
-        spec = cm.rainbow(np.linspace(0,1,g))
-        spec = np.append(spec,[[0.0, 0.0, 0.0, 1.0]],axis=0)
+    if g > 0:
+        spec = cm.rainbow(np.linspace(0, 1, g))
+        spec = np.append(spec, [[0.0, 0.0, 0.0, 1.0]], axis=0)
         return spec
     else:
-        return np.array([[1.0,0.0,0.0,1.0],[0.0, 0.0, 0.0, 1.0]])
+        return np.array([[1.0, 0.0, 0.0, 1.0], [0.0, 0.0, 0.0, 1.0]])
+
 
 spectrum = genSpectrum(gamma)
 
 if makePlots:
     # Tracer points for nothing-at-stake branching
-    trails = np.zeros((N,N),dtype=int)
-    fig = plt.figure(figsize=(16,7))
+    trails = np.zeros((N, N), dtype=int)
+    fig = plt.figure(figsize=(16, 7))
     subPlotLeft = fig.add_subplot(121)
     subPlotRight = fig.add_subplot(122)
 
-# Empty string with genesis prefix
-# rows are branches, columns are parent slot / block number
-branches = np.zeros((1, 2), dtype=int)
-honestHeads = np.array([[0,1.0-alpha,0]])
+
+if useChallenger:
+    # Empty string with genesis prefix
+    # rows are branches, columns are parent slot / block number / reserve
+    branches = np.zeros((1, 3), dtype=int)
+    honestHeads = np.array([[0, 1.0 - alpha, 0]])
+else:
+    # Empty string with genesis prefix
+    # rows are branches, columns are parent slot / block number
+    branches = np.zeros((1, 2), dtype=int)
 
 i = 0
 for y in ys:
     if makePlots:
-        if gamma>0:
-            if y > phi(gamma,alpha):
+        if gamma > 0:
+            if y > phi(gamma, alpha):
                 area.append(1.0)
                 colors.append(spectrum[gamma])
-                subPlotLeft.scatter(x[i], x[i], s=1.0, c=spectrum[gamma].reshape(1,-1))
+                subPlotLeft.scatter(slots[i], slots[i], s=1.0, c=spectrum[gamma].reshape(1, -1))
             else:
                 j = gamma
-                while y > phi(gamma-j,alpha):
+                while y > phi(gamma - j, alpha):
                     j = j - 1
-                area.append(2.0*j)
-                colors.append(spectrum[gamma-j])
-                subPlotLeft.scatter(x[i], x[i], s=j, c=spectrum[gamma-j].reshape(1,-1),alpha=0.5)
+                area.append(2.0 * j)
+                colors.append(spectrum[gamma - j])
+                subPlotLeft.scatter(slots[i], slots[i], s=j, c=spectrum[gamma - j].reshape(1, -1), alpha=0.5)
         else:
-            if y > phi(1,alpha):
+            if y > phi(1, alpha):
                 area.append(1.0)
                 colors.append(spectrum[1])
-                subPlotLeft.scatter(x[i], x[i], s=1.0, c=spectrum[1].reshape(1,-1))
+                subPlotLeft.scatter(slots[i], slots[i], s=1.0, c=spectrum[1].reshape(1, -1))
             else:
                 area.append(25.0)
                 colors.append(spectrum[0])
-                subPlotLeft.scatter(x[i], x[i], s=25.0, c=spectrum[0].reshape(1,-1),alpha=0.5)
+                subPlotLeft.scatter(slots[i], slots[i], s=25.0, c=spectrum[0].reshape(1, -1), alpha=0.5)
+
     newParents = []
     for branch in branches:
         oldParent = branch[0]
-        if y < phi(x[i]-branch[0],alpha):
-            newParents.append([branch[0],branch[1]])
-            branch[0] = x[i]
-            branch[1] = branch[1]+1
-            if makePlots:
-                subPlotLeft.plot([x[i],x[i]],[oldParent,branch[0]])
-        if makePlots:
-            if gamma>0:
-                trails[oldParent,int(x[i])] = int(min(x[i]-oldParent,gamma+1))
+        if y < phi(slots[i] - branch[0], alpha):
+            if useChallenger:
+                newParents.append([branch[0], branch[1], branch[2]])
             else:
-                trails[oldParent,int(x[i])] = int(x[i]-oldParent)
-    def honestTest():
-        for head in honestHeads:
-            if np.random.uniform(0.0,1.0) < phi(x[i]-head[0],head[1]):
-                newParents.append([x[i],int(head[2]+1)])
-                if makePlots:
-                    subPlotLeft.plot([x[i],x[i]],[oldParent,x[i]])
-                return True
-        return False
-    test = honestTest()
+                newParents.append([branch[0], branch[1]])
+            branch[0] = slots[i]
+            branch[1] = branch[1] + 1
+            if useChallenger:
+                branch[2] = branch[1] + 1
+            if makePlots:
+                subPlotLeft.plot([slots[i], slots[i]], [oldParent, branch[0]])
+        if makePlots:
+            if gamma > 0:
+                trails[oldParent, int(slots[i])] = int(min(slots[i] - oldParent, gamma + 1))
+            else:
+                trails[oldParent, int(slots[i])] = int(slots[i] - oldParent)
+
+    if useChallenger:
+        def honestTest():
+            for head in honestHeads:
+                if np.random.uniform(0.0, 1.0) < phi(slots[i] - head[0], head[1]):
+                    newParents.append([slots[i], int(head[2] + 1), 0])
+                    if makePlots:
+                        subPlotLeft.plot([slots[i], slots[i]], [oldParent, slots[i]])
+                    return True
+            return False
+    if useChallenger:
+        test = honestTest()
     for entry in newParents:
         branches = np.vstack([branches, entry])
     branches = np.unique(branches, axis=0)
@@ -129,23 +151,35 @@ for y in ys:
     for branch in branches:
         slotSet.add(branch[0])
     branchMaxBlockNumber = {x: -1 for x in slotSet}
+    if useChallenger:
+        branchReserve = {x: -1 for x in slotSet}
     maxBlockNumber = 0
     for slot in slotSet:
         for entry in branches:
             if entry[0] == slot:
                 if entry[1] > branchMaxBlockNumber[slot]:
+                    if useChallenger:
+                        branchReserve[slot] = entry[2]
                     branchMaxBlockNumber[slot] = entry[1]
-                    maxBlockNumber = max(maxBlockNumber,branchMaxBlockNumber[slot])
-    branches = np.array(list(filter(lambda x: maxBlockNumber - x[1] < branchDepth, list(branchMaxBlockNumber.items()))))
-    if test:
-        newHeads = []
-        for branch in branches:
-            if branch[1] == maxBlockNumber:
-                newHeads.append(branch)
-        honestHeads = np.empty([1,3])
-        for newHead in newHeads:
-            honestHeads = np.vstack([honestHeads,[newHead[0],(1.0-alpha)/len(newHeads),newHead[1]]])
-    i = i+1
+                    maxBlockNumber = max(maxBlockNumber, branchMaxBlockNumber[slot])
+
+    if useChallenger:
+        def addReserve(b):
+            return np.concatenate((np.asarray(b),[branchReserve[b[0]]]),axis=0)
+        branches = np.array(list(map(addReserve,list(filter(lambda x: maxBlockNumber - x[1] < branchDepth,list(branchMaxBlockNumber.items()))))))
+    else:
+        branches = np.array(list(filter(lambda x: maxBlockNumber - x[1] < branchDepth,list(branchMaxBlockNumber.items()))))
+
+    if useChallenger:
+        if test:
+            newHeads = []
+            for branch in branches:
+                if branch[1] == maxBlockNumber:
+                    newHeads.append(branch)
+            honestHeads = np.empty([1, 3])
+            for newHead in newHeads:
+                honestHeads = np.vstack([honestHeads, [newHead[0], (1.0 - alpha) / len(newHeads), newHead[1]]])
+    i = i + 1
 
 # Plot the tracer points of all branches
 if makePlots:
@@ -153,35 +187,36 @@ if makePlots:
     j = 0
     for n in np.arange(N):
         for m in np.arange(N):
-            if gamma>0:
-                if trails[i,j] < gamma+1 and trails[i,j] > 0:
-                    subPlotLeft.scatter(n,m,s=trails[i,j],c=spectrum[trails[i,j]-1].reshape(1,-1),alpha=0.5)
-                if trails[i,j] > gamma:
-                    subPlotLeft.scatter(n,m,s=int(gamma*fb/fa),c=spectrum[int(gamma*fb/fa)].reshape(1,-1),alpha=0.5)
+            if gamma > 0:
+                if trails[i, j] < gamma + 1 and trails[i, j] > 0:
+                    subPlotLeft.scatter(n, m, s=trails[i, j], c=spectrum[trails[i, j] - 1].reshape(1, -1), alpha=0.5)
+                if trails[i, j] > gamma:
+                    subPlotLeft.scatter(n, m, s=int(gamma * fb / fa), c=spectrum[int(gamma * fb / fa)].reshape(1, -1),
+                                        alpha=0.5)
             else:
-                if trails[i,j] > 0:
-                    subPlotLeft.scatter(n,m,s=1.0,c=spectrum[1].reshape(1,-1),alpha=0.5)
-            i = i+1
-        j = j+1
+                if trails[i, j] > 0:
+                    subPlotLeft.scatter(n, m, s=1.0, c=spectrum[1].reshape(1, -1), alpha=0.5)
+            i = i + 1
+        j = j + 1
         i = 0
-    subPlotRight.scatter(x, ys, s=area, c=colors)
+    subPlotRight.scatter(slots, ys, s=area, c=colors)
     subPlotRight.set_xlabel("Slot")
     subPlotRight.set_ylabel("Y")
-    subPlotRight.set_ylim([0.0,1.0])
-    subPlotRight.set_title("Nonce distribution, "+r'$\alpha$ = '+str(alpha))
+    subPlotRight.set_ylim([0.0, 1.0])
+    subPlotRight.set_title("Nonce distribution, " + r'$\alpha$ = ' + str(alpha))
 
 maxL = 0
 numBranch = 0
 for branch in branches:
-    maxL = max(branch[1],maxL)
-    numBranch = numBranch+1
-hudStr = ("Maximum block number: "+str(maxL))+("\nFinal number of branches: "+str(numBranch))+("\nParent slot / block number pairs:\n")+str(branches)
+    maxL = max(branch[1], maxL)
+    numBranch = numBranch + 1
+hudStr = ("Maximum block number: " + str(maxL)) + ("\nFinal number of branches: " + str(numBranch)) + (
+    "\nParent slot / block number pairs:\n") + str(branches)
 if makePlots:
     subPlotLeft.text(0.01, 0.99, hudStr, ha='left', va='top', transform=subPlotLeft.transAxes)
     subPlotLeft.set_xlabel("Slot")
     subPlotLeft.set_ylabel("Parent slot")
-    subPlotLeft.set_title("Branching diagram, depth = "+str(branchDepth))
+    subPlotLeft.set_title("Branching diagram, depth = " + str(branchDepth))
     plt.show()
 else:
     print(hudStr)
-
